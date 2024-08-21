671531f21ec48ff4c6e578a386800c4713d0af31
NISCHAY HR <nischayhr11@gmail.com>
2024-08-21 13:32:03 +0530

    Delete IndexJS/index.js

--- IndexJS/index.js
+++
@@ -1,9 +0,0 @@
-// This File Return The Object To the Current Directory (stores array of Exports object Of All The JS Files In The Directory).
-
-let a=require("./apple");
-let b=require("./banana");
-let c=require("./mango");
-
-let arr=[a,b,c];
-
-module.exports=arr;
