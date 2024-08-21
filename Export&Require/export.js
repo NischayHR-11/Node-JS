4b48adcb8a2926c320a7646a25966966831ab70b
NISCHAY HR <nischayhr11@gmail.com>
2024-08-21 13:03:15 +0530

    Exporting Object  Example File Created

---
+++ Export&Require/export.js
@@ -0,0 +1,19 @@
+// Module Exports Object
+
+function sum(a,b){
+    return a+b;
+}
+
+function product(a,b){
+
+    return a*b;
+}
+
+let pi=3.144;
+
+module.exports ={
+
+    sum:sum,
+    prod:product,
+    pi:pi
+};
